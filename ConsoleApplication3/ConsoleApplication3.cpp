e5660396c655fc4898dab39d7109e68b316daab1
Egor Fedorov <egorfedorov0306@gmail.com>
2024-11-26 18:56:21 +0200

    new gitignore

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -1,6 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
 int main() {
-	cout << "Dasha + Egor = love";
+	bool DashaLoveEgor = true;
+	cout << "Dahsa love's Egor?";
+	cin >> DashaLoveEgor;
+	if (DashaLoveEgor == true) {
+		cout << "Dasha + Egor = love";
+	}
+	else {
+		cout << "it's incorrect answer";
+	}
 	return 0;
 }
